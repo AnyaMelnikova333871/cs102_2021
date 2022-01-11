cc95021de321d64cc7ba6658f9bf188255d4049f
Анна Мельникова 333871 <90198133+AnyaMelnikova333871@users.noreply.github.com>
2022-01-11 15:56:40 +0300

    black good meow

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -23,7 +23,7 @@ def is_prime(n: int) -> bool:
     return True
 
 
-#print (is_prime(1))
+# print (is_prime(1))
 
 
 def gcd(a: int, b: int) -> int:
@@ -66,7 +66,9 @@ def multiplicative_inverse(e: int, phi: int) -> int:
 # print(multiplicative_inverse(7, 40)) # -> 23
 
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(
+    p: int, q: int
+) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:

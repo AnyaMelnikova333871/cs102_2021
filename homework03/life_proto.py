20fa4c0c3756702b5019369487f57a86277ed7df
Анна Мельникова 333871 <90198133+AnyaMelnikova333871@users.noreply.github.com>
2022-01-15 22:51:48 +0300

    the commit

--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -1,6 +1,7 @@
+import copy
 import random
 import typing as tp
-import copy
+
 import pygame
 from pygame.locals import *
 
@@ -32,14 +33,18 @@ class GameOfLife:
         self.speed = speed
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+            pygame.draw.line(
+                self.screen, pygame.Color("black"), (x, 0), (x, self.height)
+            )
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
+            pygame.draw.line(
+                self.screen, pygame.Color("black"), (0, y), (self.width, y)
+            )
 
     def run(self) -> None:
-        """ Запустить игру """
+        """Запустить игру"""
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
@@ -66,9 +71,6 @@ class GameOfLife:
         pygame.quit()
 
     def create_grid(self, randomize: bool = False) -> Grid:
-
-
-
 
         """
         Клетка считается живой, если ее значение равно 1, в противном случае клетка
@@ -102,11 +104,21 @@ class GameOfLife:
         Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
         """
         grid = self.create_grid(self)
-        for i in range (len(grid)):
+        for i in range(len(grid)):
             for j in range(len(grid[0])):
-                rect = (self.cell_size*j, self.cell_size*i, self.cell_size,  self.cell_size)
-                pygame.draw.rect(self.screen, pygame.Color('green') if self.grid[i][j] == 1 else pygame.Color('white'), rect)
-
+                rect = (
+                    self.cell_size * j,
+                    self.cell_size * i,
+                    self.cell_size,
+                    self.cell_size,
+                )
+                pygame.draw.rect(
+                    self.screen,
+                    pygame.Color("green")
+                    if self.grid[i][j] == 1
+                    else pygame.Color("white"),
+                    rect,
+                )
 
     def get_neighbours(self, cell: Cell) -> Cells:
         """
@@ -177,9 +189,8 @@ class GameOfLife:
         return self.grid
 
 
-
-
 from pprint import pprint as pp
+
 game = GameOfLife(320, 240, 40, 1)
 
 game.run()

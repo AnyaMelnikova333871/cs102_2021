4f2549448a493c0f84678c547a3fcd1cf4eefe39
Анна Мельникова 333871 <90198133+AnyaMelnikova333871@users.noreply.github.com>
2022-01-19 14:21:48 +0300

    13

--- homework04_new/maze.py
+++ homework04_new/maze.py
@@ -240,7 +240,7 @@ def solve_maze(
     while grid[a][b] == 0:
         make_step(grid, k)
         k += 1
-    grid, dawae = shortest_path(grid, (a, b))
+    grid, dawae = shortest_path(int(grid, (a, b)))
     return grid, theway
 
 
